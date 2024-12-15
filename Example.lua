350d43d16f79b202809762ad444e3f8beb2fd5d1
LionTheGreatRealFrFr <topgtatey@gmail.com>
2024-12-15 21:05:36 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -95,23 +95,13 @@ Toggles.MyToggle:SetValue(false)
     You can call :AddButton on a button to add a SubButton!
 ]]
 
-local MyButton = LeftGroupBox:AddButton({
-    Text = 'Button',
-    Func = function()
-        print('You clicked a button!')
-    end,
-    DoubleClick = false,
-    Tooltip = 'This is the main button'
-})
+local MyButton = LeftGroupBox:AddButton('Button', function()
+    print('You clicked a button!')
+end)
 
-local MyButton2 = MyButton:AddButton({
-    Text = 'Sub button',
-    Func = function()
-        print('You clicked a sub button!')
-    end,
-    DoubleClick = true, -- You will have to click this button twice to trigger the callback
-    Tooltip = 'This is the sub button (double click me!)'
-})
+local MyButton2 = MyButton:AddButton('Sub button', function()
+    print('You clicked a sub button!')
+end)
 
 --[[
     NOTE: You can chain the button methods!
