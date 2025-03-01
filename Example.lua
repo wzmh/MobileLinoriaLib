6e5b670b74254dc2e1bbfc8837f05c8e755e41f7
aladin <micheljaheim5@proton.me>
2025-03-01 10:47:18 -0500

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -431,8 +431,8 @@ SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
 -- use case for doing it this way:
 -- a script hub could have themes in a global folder
 -- and game configs in a separate folder per game
-ThemeManager:SetFolder('MyScriptHub')
-SaveManager:SetFolder('MyScriptHub/specific-game')
+ThemeManager:SetFolder('iue9')
+SaveManager:SetFolder('iue9/cfgs')
 
 -- Builds our config menu on the right side of our tab
 SaveManager:BuildConfigSection(Tabs['UI Settings'])
